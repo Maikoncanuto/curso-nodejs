74df3332287a958087e5a645a65f028c3aa15ab7
Maikon Canuto <maikoncanuto@gmail.com>
2016-08-22 13:06:16 -0300

    modification in app.js, configuration ejs

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 var express = require("express")();
 var porta = 3000;
 
+//configurando EJS 
+express.set('view engine', 'ejs');
+
 //rotas
 express.get("/", function(req, res){
     res.end("<html><body>Home do site</body></html>");
 });
 express.get("/produtos", function(req, res){
-    res.end("<html><body><h1>Listagem de produtos</h1></body></html>");
+    res.render("produtos/lista");
 });
 
 //servidor
