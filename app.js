63f55582cefb1fdd5db2877c248715242e0d8804
Maikon Canuto <maikoncanuto@gmail.com>
2016-08-25 14:15:15 -0300

    Alterando app.js

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 //importações e configurações
-var express = require("express")();
+var app = require("./config/express")();
+
+//Importação das rotas
+var routeProdutos = require('./app/routes/routeProdutos.js')(app);
+
+//Configuraçõa do servidor
 var porta = 3000;
 
-//configurando EJS 
-express.set('view engine', 'ejs');
-
 //rotas
-express.get("/", function(req, res){
+app.get("/", function(req, res){
     res.end("<html><body>Home do site</body></html>");
-});
-express.get("/produtos", function(req, res){
-    res.render("produtos/lista");
 });
 
 //servidor
-express.listen(porta, function(){
+app.listen(porta, function(){
     console.log("Servidor executando...");
     console.log("Porta: " + porta);
     console.log("IP: localhost");
