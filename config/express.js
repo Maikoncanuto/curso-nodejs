0489d10ed0793d2e11c1f9ba9cfc01e5b464e1d3
Maikon Canuto <maikoncanuto@gmail.com>
2016-08-25 14:50:50 -0300

    Configurando express para pasta views

--- config/express.js
+++ config/express.js
@@ -3,6 +3,7 @@ var app = require('express')();
 
 //Configuração do Express e modulos
 app.set('view engine', 'ejs');
+app.set('views', './app/views'); //configura o express a procurar a pasta views
 
 //Retorna express configurado
 module.exports = function(){
