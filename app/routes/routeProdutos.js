0ae38c6aaf839d6b39f773e59aa23c8b485f4924
Maikon Canuto <maikoncanuto@gmail.com>
2016-08-26 00:05:48 -0300

    Configurando Mysql e testando query select

--- app/routes/routeProdutos.js
+++ app/routes/routeProdutos.js
@@ -1,6 +1,30 @@
 //Configuração da rota produtos
 module.exports = function(app){
     app.get("/produtos", function(req, res){
-        res.render("produtos/lista");
+
+        //importação do mysql
+        var mysql = require('mysql');
+
+        //Configuração do mysql para conexão
+        var connection = mysql.createConnection({
+            host : 'localhost', //endereço da conexão
+            user : 'root', // usuario da conexão
+            password : '123456789', //senha da conexão
+            database : 'casadocodigo_nodejs' //banco de dados
+        });
+
+        //consulta, function(primeiro argumento é o erro, segundo é o resultado)
+        connection.query('select * from livros', function(err, results){
+            res.send(results);
+        });
+
+        //Fecha conexão do mysql
+        connection.end();
+
+
+
+
+        //renderizar a página
+        //res.render("produtos/lista");
     });
 }
